cf2e75d425a330c6c96bdd851b7f6b0223b588f4
Tu Nguyen <wz257@stud.uni-hiedelberg.de>
2020-11-25 20:53:40 +0100

    Dies ist eine commit message.

---
+++ potenzen.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+
+namespace power{
+	int iterative(int q, int n)
+	{
+		int result = 1;
+		std::cout << result << std::endl;
+		while(n >0)
+		{		
+			result = result * q;
+			std::cout << result << std::endl;
+			n = n- 1;
+		}
+			return result;
+	}
+	
+}
+
+int main(int argc, char** argv)
+{
+	int basis;
+	int number;
+	std::cout << "Geben Sie eine Basis für die Potenz ein." << std::endl;
+	std::cout << " q = " << std::flush;
+	std::cin >> basis;
+	std::cout << "Geben Sie eine Potenz ein." << std::endl;
+	std::cout << "n = " << std::flush;
+	std::cin >> number;
+	power::iterative(basis,number);
+}
